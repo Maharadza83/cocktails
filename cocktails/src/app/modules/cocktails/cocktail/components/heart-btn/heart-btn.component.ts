172a27808449a1e98d2478b6cbe39dc70d9a60a4
Dominik <d.malczynski.519@studms.ug.edu.pl>
2024-01-21 00:42:58 +0100

    REMOVE LOTTIE

--- cocktails/src/app/modules/cocktails/cocktail/components/heart-btn/heart-btn.component.ts
+++ cocktails/src/app/modules/cocktails/cocktail/components/heart-btn/heart-btn.component.ts
@@ -1,6 +1,5 @@
-import {ChangeDetectionStrategy, Component, inject, Input, NgZone, OnInit, signal, WritableSignal} from '@angular/core';
-import {AnimationOptions, LottieComponent} from "ngx-lottie";
-import {AnimationItem} from "ngx-lottie/lib/symbols";
+import {ChangeDetectionStrategy, Component, inject, Input, OnInit, signal, WritableSignal} from '@angular/core';
+import {} from "ngx-lottie";
 import {MyFavouritesService} from "../../../../../services/my-favourites.service";
 import {Cocktail} from "../../../../../models/cocktail";
 import {NgTemplateOutlet} from "@angular/common";
@@ -10,7 +9,6 @@ import {LoaderComponent} from "../../../../../shared/loader/loader.component";
   selector: 'app-heart-btn',
   standalone: true,
   imports: [
-    LottieComponent,
     NgTemplateOutlet,
     LoaderComponent
   ],
@@ -22,26 +20,9 @@ export class HeartBtnComponent implements OnInit {
   @Input() public cocktail: Cocktail
 
   private readonly myFavouritesService: MyFavouritesService = inject(MyFavouritesService);
-  private readonly ngZone: NgZone = inject(NgZone);
 
   public isFav: WritableSignal<boolean> = signal(false);
   public loading: WritableSignal<boolean> = signal(false)
-
-  public readonly optionsAdd: AnimationOptions = {
-    path: 'assets/lottie/heart.json',
-    loop: false,
-    autoplay: false,
-    initialSegment: [ 24, 5 ],
-  };
-
-  public readonly optionsRemove: AnimationOptions = {
-    path: 'assets/lottie/heart.json',
-    loop: false,
-    autoplay: false,
-    initialSegment: [ 5, 24 ],
-  };
-
-  private animationItem: AnimationItem = null;
 
   public ngOnInit(): void {
     this.loading.set(true)
@@ -51,16 +32,9 @@ export class HeartBtnComponent implements OnInit {
     })
   }
 
-  public animationCreated(animationItem: AnimationItem): void {
-    this.animationItem = animationItem;
-  }
-
   public addToFav(): void {
     this.loading.set(true)
     this.myFavouritesService.addToFav(this.cocktail).subscribe(() => {
-      this.ngZone.runOutsideAngular(() => {
-        this.animationItem.play();
-      });
         this.loading.set(false)
         this.isFav.set(true)
     })
@@ -69,9 +43,6 @@ export class HeartBtnComponent implements OnInit {
   public removeFromFav(): void {
     this.loading.set(true)
     this.myFavouritesService.removeFromFav(this.cocktail.idDrink).subscribe(() => {
-      this.ngZone.runOutsideAngular(() => {
-        this.animationItem.play();
-      });
         this.loading.set(false)
         this.isFav.set(false)
     })
